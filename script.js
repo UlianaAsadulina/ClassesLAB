8872a4c774607410a981a089417110c8b404fc02
UlianaAsadulina <u.asadulina@icloud.com>
2024-12-17 14:29:32 -0500

    re-create Robin using the Character class

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-// Part 1: Humble Beginnings
+// -----------------------Part 1: Humble Beginnings
+
 // Let’s model a simple adventurer with basic properties such as health and an inventory. 
 // We will call the adventurer “Robin.”
 
@@ -18,9 +19,12 @@
 //      The companion’s type is “Flea.”
 //      The companion has its own belongings, which includes a small hat and sunglasses.
 
+/**
+ * 
+
 const adventurer = {
     name: "Robin",
-    health: 10,
+   health: 10,
     inventory: ["sword", "potion", "artifact"],
     companion: {
         name: "Leo",
@@ -44,3 +48,40 @@ adventurer.roll();
 adventurer.roll();
 
 
+ */
+
+// ----------------------------Part 2: Class Fantasy
+
+
+// Start with a Character class, which will define generic character entities. 
+// Robin and their companions all have a name, so the Character class should definitely include name as one of its properties. 
+// At this stage, we will also make the decision that every character should have health (which we will standardize 
+// to a maximum of 100, and an inventory (even if the inventory is empty).
+
+// Here is what the basic Character class looks like so far, including a constructor function that 
+// allows us to create new characters with whatever name we would like:
+
+class Character {
+  constructor (name) {
+    this.name = name;
+    this.health = 100;
+    this.inventory = [];
+  }
+  roll (mod = 0) {
+    const result = Math.floor(Math.random() * 20) + 1 + mod;
+    console.log(`${this.name} rolled a ${result}.`)
+    }
+}
+
+// Every character should also be able to make rolls. 
+// Add the roll method to the Character class.
+
+// Now, we can re-create Robin using the Character class!
+
+const robin = new Character("Robin");
+robin.inventory = ["sword", "potion", "artifact"];
+robin.companion = new Character("Leo");
+robin.companion.type = "Cat";
+robin.companion.companion = new Character("Frank");
+robin.companion.companion.type = "Flea";
+robin.companion.companion.inventory = ["small hat", "sunglasses"];
